f7fa5d8405776dbb9e884de39b38d7785310f9c3
Scott Schwab <schwabpiano@gmail.com>
2022-05-26 06:44:40 -0500

    Fully functional but with lots of repeated code

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const pictureFrame = document.querySelector("#picture-frame");
 
 let slides = document.querySelectorAll(".slide");
 let currentSlide = 0;
-
 pictureFrame.appendChild(slides[currentSlide]);
 
 const nextBtn = document.querySelector("#next-btn");
@@ -13,11 +12,10 @@ nextBtn.addEventListener('click', () => {
     } else {
         currentSlide = 0;
     }
-    pictureFrame.appendChild(slides[currentSlide]);
+    changeSlide(currentSlide);
 });
 
 const prevBtn = document.querySelector("#prev-btn");
-
 prevBtn.addEventListener('click', () => {
     pictureFrame.removeChild(slides[currentSlide]);
     if (currentSlide !== 0) {
@@ -25,7 +23,52 @@ prevBtn.addEventListener('click', () => {
     } else {
         currentSlide = slides.length - 1;
     }
-    pictureFrame.appendChild(slides[currentSlide]);
+    changeSlide(currentSlide);
 });
+
+const changeSlide = () => {
+    circles.forEach(circle => {
+        circle.setAttribute("style", "background-color: none;");
+    });
+    pictureFrame.appendChild(slides[currentSlide]);
+    let currentImg = slides[currentSlide].firstElementChild;
+    currentImg.classList.add("animate-slide");
+    circlesArray[currentSlide].setAttribute("style", "background-color: red;");
+}
+
+// navigation circles
+
+const selectionCircles = document.querySelector("#selection-circles");
+let circlesArray = [];
+
+for (let i = 0; i < slides.length; i++) {
+    let circle = document.createElement("div");
+    circle.classList.add("circle");
+    selectionCircles.appendChild(circle);
+    circlesArray.push(circle);
+}
+
+let circles = document.querySelectorAll(".circle");
+circles[0].setAttribute("style", "background-color: red;");
+circles.forEach(circle => {
+    circle.addEventListener('click', () => {
+        pictureFrame.removeChild(slides[currentSlide]);
+        currentSlide = circlesArray.indexOf(circle);
+        changeSlide(currentSlide);
+    });
+});
+
+const advanceSlideTimer = () => {
+    pictureFrame.removeChild(slides[currentSlide]);
+    if (currentSlide === slides.length - 1) {
+        currentSlide = 0;
+    } else {
+        currentSlide += 1;
+    }
+ 
+    changeSlide(currentSlide);
+}
+
+setInterval(advanceSlideTimer, 5000);
 
 
